f72befc13c314090a41b8e408c68bc0f0902b079
Yves Pascal Wecker <105780910+YvesPascalWecker@users.noreply.github.com>
2026-01-31 11:16:23 +0100

    Add: Menu Targets; EntryPoint for enemies

--- visible/code/menue_controller.gd
+++ visible/code/menue_controller.gd
@@ -2,25 +2,28 @@ class_name MenueController
 extends Control
 
 @export_group("Scean Target")
-@export var start_scean:PackedScene ##Inharitate the targetet Start Scene e.g. the first level
-@export var option_scean:PackedScene ##Inharitate the targetet Option Scene
+@export var start_scean : PackedScene ##Inharitate the targetet Start Scene e.g. the first level
+@export var option_scean : PackedScene  ##Inharitate the targetet Option Scene
 @export_group("")
+
 
 func _ready() -> void:
 	%SplashScreen.visible = true
 	%"V-SortStartMenue".visible = false
+
 
 func _process(_delta: float) -> void:
 	if %SplashScreen.visible and Input.is_anything_pressed():
 		%SplashScreen.visible = false
 		%"V-SortStartMenue".visible = true
 
+
 func _on_start_button_pressed() -> void:
-	pass
+	get_tree().change_scene_to_file(start_scean.resource_path)
 
 
 func _on_options_button_pressed() -> void:
-	pass
+	get_tree().change_scene_to_file(option_scean.resource_path)
 
 
 func _on_end_button_pressed() -> void:
